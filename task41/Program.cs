7c6915d2a0c0faf0daae4b538ddfba05d41667e6
School47 <School47@gmail.com>
2023-04-10 15:52:31 +0500

    41+43

---
+++ task41/Program.cs
@@ -0,0 +1,34 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел.
+//  Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// -1, -7, 567, 89, 223-> 3
+
+Console.WriteLine($"Количество чисел(М), которые вы будете вводить:");
+int m = Convert.ToInt32(Console.ReadLine());
+int[] array = InputDataInArray(m);
+int result = CountNumbers(array, 0);
+Console.WriteLine(result);
+
+
+int[] InputDataInArray(int mNumbers)
+{
+    int[] arrayForNumbers = new int[mNumbers];
+    for (int i = 0; i < mNumbers; i++)
+    {
+        Console.Write($"Введите {i + 1}-ое число:");
+        arrayForNumbers[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return arrayForNumbers;
+}
+
+int CountNumbers(int[] arrayForNumbers, int moreThanThisNumber)
+{
+    int counter = 0;
+    for (int i = 0; i < arrayForNumbers.Length; i++)
+    {
+        if (arrayForNumbers[i] > moreThanThisNumber) counter++;
+    }
+    return counter;
+}
---
+++ task43/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 43: Напишите программу, 
+// которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; 
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+double b1;
+double k1;
+double b2;
+double k2;
+double x;
+double y;
+InputData();
+x = CalculateX(b1, k1, b2, k2);
+y = CalculateY(x, k1, b1);
+Console.WriteLine($"{x}, {y}");
+
+void InputData()
+{
+    Console.WriteLine($"Введите b1:");
+    b1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Введите k1:");
+    k1 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Введите b2:");
+    b2 = Convert.ToDouble(Console.ReadLine());
+    Console.WriteLine($"Введите k2:");
+    k2 = Convert.ToDouble(Console.ReadLine());
+}
+
+double CalculateX(double b1Num, double k1Num, double b2Num, double k2Num)
+{
+    return (b2Num - b1Num) / (k1Num - k2Num);
+}
+
+double CalculateY(double xNum, double k1Num, double b1Num)
+{
+    return Convert.ToDouble(k1Num * xNum + b1Num);
+}
+
